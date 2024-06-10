9b9668e0b55e96ec25d342e9a75dd27b4512c67f
tropical_bottle <alexandre.blanquero00@gmail.com>
2024-06-10 11:29:25 +0200

    feat: add route to solo journey (test)

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,21 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SoloJourneyMenu from "@/components/SoloJourneyMenu.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
+  //TODO: Se renseigner sur la lazy loading de routes
   routes: [
     {
       path: '/',
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/play',
+      name: 'solo-journey',
+      component: SoloJourneyMenu
+    }
   ]
 })
 
