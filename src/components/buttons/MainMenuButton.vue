eb477e59ce93303e8bb6f058dda6fadf1662215e
tropical_bottle <alexandre.blanquero00@gmail.com>
2024-06-10 09:15:16 +0200

    refactor: button routable

--- src/components/buttons/MainMenuButton.vue
+++ src/components/buttons/MainMenuButton.vue
@@ -1,16 +1,24 @@
 <script setup lang="ts">
 defineProps<{
-  label: string,
+  label: string | Object,
   backgroundColor: string,
+  target?: string
 }>()
 </script>
 
 <template>
-  <div class="my-3 cursor-pointer bg-white inline-block p-1.5 rounded-md border-4 border-black scale-95 hover:scale-100 min-w-full">
+  <RouterLink v-if="typeof label === 'string'"
+       class="my-3 cursor-pointer bg-white inline-block p-1.5 rounded-md border-4 border-black scale-95 hover:scale-100" :to="`${target}`">
     <div :class="`bg-primary-${backgroundColor} px-14 py-3 rounded flex justify-center`">
-      <p class="text-4xl text-black">{{label}}</p> <!-- Rajouter couleur texte tailwind palette -->
+      <p class="text-4xl text-black">{{ label }}</p> <!-- Rajouter couleur texte tailwind palette -->
     </div>
-  </div>
+  </RouterLink>
+  <RouterLink v-else
+       class="my-3 cursor-pointer bg-white inline-block p-1.5 rounded-md border-4 border-black scale-95 hover:scale-100" :to="`${target}`">
+    <div :class="`bg-primary-${backgroundColor} px-7 py-3 rounded flex justify-center`">
+      <component :is="label" class="w-16 text-black"/>
+    </div>
+  </RouterLink>
 </template>
 
 <style scoped>
