bb7dbfd2ceb412bdd2724428994b155d60274301
tropical_bottle <alexandre.blanquero00@gmail.com>
2024-06-18 12:56:56 +0200

    feat: create game components

---
+++ src/components/game/BuffDebuff.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{
+  color: string
+}>()
+</script>
+
+<template>
+  <div :class="`bg-primary-${color} p-6 m-4`">
+    <p class="text-2xl font-bold text-gray-700">
+      Désordre
+    </p>
+    <p>Cupcake ipsum dolor sit amet croissant jelly beans pudding. Fruitcake pie fruitcake wafer I love dessert.</p>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/game/Scoreboard.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="bg-[url('src/assets/img/wood.png')] p-3 bg-no-repeat bg-cover w-full md:w-1/2 m-auto">
+    <div
+        class="bg-[url('src/assets/img/board.png')] bg-no-repeat bg-cover p-2">
+      <p class="text-center text-white font-bold text-2xl">Score: <span>352</span></p>
+      <p class="text-center text-white text-lg">round <span>3</span></p>
+      <p class="text-center text-white text-5xl"><span>3:00</span></p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
