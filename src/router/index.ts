8e3f7cad3f43e544787c681582c548cce0140564
tropical_bottle <alexandre.blanquero00@gmail.com>
2024-06-25 11:12:11 +0200

    refactor|feat: api call word

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-import SoloJourneyMenu from "@/components/SoloJourneyMenu.vue";
-import Shop from "@/components/Shop.vue";
-import Game from "@/components/Game.vue";
+import SoloJourneyMenu from "@/views/SoloJourneyMenuView.vue";
+import Shop from "@/views/ShopView.vue";
+import Game from "@/views/GameView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
