e3ea79c6cca4b939a38dc6655b31c08fb8de08f0
tropical_bottle <alexandre.blanquero00@gmail.com>
2024-06-27 12:37:46 +0200

    feat: get boss for journey

--- src/views/SoloJourneyMenuView.vue
+++ src/views/SoloJourneyMenuView.vue
@@ -1,19 +1,44 @@
 <script setup lang="ts">
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+import Card from '@/components/cards/Card.vue';
+import MainMenuButton from '@/components/buttons/MainMenuButton.vue';
+import { ArrowLeftIcon } from '@heroicons/vue/24/outline';
+import { RouterLink } from 'vue-router';
 
-import Card from "@/components/cards/Card.vue";
-import MainMenuButton from "@/components/buttons/MainMenuButton.vue";
-import {ArrowLeftIcon} from "@heroicons/vue/24/outline";
+interface Boss {
+  id: number;
+  name: string;
+  description: string;
+  default_life: number;
+}
+
+const bosses = ref<Boss[]>([]);
+
+onMounted(async () => {
+  bosses.value = await getBosses();
+});
+
+async function getBosses(): Promise<Boss[]> {
+  try {
+    const response = await axios.get<Boss[]>('http://localhost:3000/boss/three');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching bosses:', error);
+    return [];
+  }
+}
 </script>
 
 <template>
-  <MainMenuButton background-color="transparent" target="/" :label="ArrowLeftIcon" class="absolute top-3 left-32" />
-  <div class="flex flex-row">
-    <Card title="Souper-Capy" category="capybara" img-url="CapyHero.png" :difficulty="1"/>
-    <Card title="Anxious Possum" category="possum" img-url="AnxiousPossum.png" :difficulty="2"/>
-    <Card title="Anxious Possum" category="possum" img-url="AnxiousPossum.png" :difficulty="3"/>
-    <RouterLink to="shop">
-      <Card class="h-full" title="It's shopping time!" category="shop" img-url="Shop.png"/>
-    </RouterLink>
+  <div>
+    <MainMenuButton background-color="transparent" target="/" :label="ArrowLeftIcon" class="absolute top-3 left-32" />
+    <div class="flex flex-row">
+      <Card v-for="boss in bosses" :key="boss.id" :title="boss.name" category="capybara" img-url="CapyHero.png" :difficulty="1"/>
+      <RouterLink to="shop">
+        <Card class="h-full" title="It's shopping time!" category="shop" img-url="Shop.png"/>
+      </RouterLink>
+    </div>
   </div>
 </template>
 
